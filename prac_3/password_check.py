7bd189fd696ed31a1b4b840bc9a3d1fa5b71fa8d
Doa Roys <doa.roys@my.jcu.edu.au>
2021-03-21 21:50:27 +1000

    Convert temperatures in Farenheit to Celsius and vice versa

--- prac_3/password_check.py
+++ prac_3/password_check.py
@@ -1,11 +1,23 @@
 MIN_LENGTH = 8
-password = input("Enter password: ")
-valid_input = False
-while not valid_input:
- if len(password)<MIN_LENGTH:
-    print("Enter a password with atleast {} characters".format(MIN_LENGTH))
-    password = input ( "Enter password: " )
- else:
-    for i in range(len(password)):
-        print("*",end='')
-    valid_input = True
+def main() :
+    get_password ()
+  # valid_input = False
+
+#def get_password()
+
+   password = get_password ()
+ACTUAL_LENGTH = len(password)
+
+def print_asterisks():
+    def print_asterisk(ACTUAL_LENGTH)
+
+#while not valid_input :
+    if ACTUAL_LENGTH < MIN_LENGTH :
+        print ( "Enter a password with atleast {} characters".format ( MIN_LENGTH ) )
+        password = input ( "Enter password: " )
+    else :
+    def print_asterisk(ACTUAL_LENGTH)
+        for i in range ( len ( password ) ) :
+            print ( "*", end='' )
+        valid_input = True
+main ()
