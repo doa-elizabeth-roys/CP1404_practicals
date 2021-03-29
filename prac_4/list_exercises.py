30b86cc7aec4a93b7f7c31e64b4d19aecd30889c
Doa Roys <doa.roys@my.jcu.edu.au>
2021-03-29 13:36:31 +1000

    Add a security checker

--- prac_4/list_exercises.py
+++ prac_4/list_exercises.py
@@ -15,3 +15,12 @@ print("The largest number is {}".format(max(numbers)))
 print("The smallest number is {}".format(min(numbers)))
 print("The average of the numbers is {}".format((sum(numbers)/len(numbers))))
 
+#security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+input_username = input("Enter your username: ")
+for j in range(len(usernames)):
+ if input_username == usernames[j]:
+     print("Access granted")
+     break
+ else:
+     print("Access denied")
