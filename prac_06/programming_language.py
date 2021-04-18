1ac3e6f210ee3ab2cf9729861b93f8954c3080b1
Doa Roys <doa.roys@my.jcu.edu.au>
2021-04-18 12:44:56 +1000

    Print the python object.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,29 @@
+class ProgrammingLanguage:
+    """Programming Languages Comparison."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Returns True / False if the programming language is dynamically typed or not."""
+        return self.typing == "Dynamic"
+
+    def _str_(self):
+        """Return a string with following format."""
+        return ("{}, {} Typing, Reflection ={}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                             self.year))
+
+
+# Loop through and print the names of all of the languages with dynamic typing
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+languages = [ruby, python, visual_basic]
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
