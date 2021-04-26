1ebb52dd9ab55bce0c3958fdf6f317cf59c172d6
Doa Roys <doa.roys@my.jcu.edu.au>
2021-04-26 19:22:36 +1000

    Modified the return in str method.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,7 +2,7 @@ class ProgrammingLanguage:
     """Programming Languages Comparison."""
 
     def __init__(self, name, typing, reflection, year):
-        """Initialise a instance."""
+        """Initialise instance of Programming languages."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
@@ -14,15 +14,16 @@ class ProgrammingLanguage:
 
     def _str_(self):
         """Return a string with following format."""
-        return ("{}, {} Typing, Reflection ={}, First appeared in {}".format(self.name, self.typing, self.reflection,
-                                                                             self.year))
+        return ("{x.name}, {x.typing} Typing, Reflection ={x.reflection}, First appeared in {x.year}".format(x=self))
 
 # Loop through and print the names of all of the languages with dynamic typing
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
 languages = [ruby, python, visual_basic]
+
 print("The dynamically typed languages are:")
 for language in languages:
-    if language.is_dynamic():
+   if language.is_dynamic():
         print(language.name)
