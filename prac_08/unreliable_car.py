7eb4c07df974257c20b928d1bffc0fab05d657e7
Doa Roys <doa.roys@my.jcu.edu.au>
2021-05-09 15:56:23 +1000

    Make a derived class called UnreliableCar that inherits from Car.

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,26 @@
+from prac_08.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes fare costs."""
+
+    def __init__(self, name, fuel):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = random.uniform(0, 100)
+
+    def drive(self, number):
+        """Drive the car if random number is less
+         than the car's reliability.
+        """
+        if number < self.reliability:
+            self.distance = self.fuel
+            self.fuel = 0
+        else:
+            self.distance = 0
+        return self.distance
+
+    def _str_(self):
+        """Return the car's name,fuel and odometer reading"""
+        return ("{} has {} unit fuel and has travelled {} distance".format(self.name, self.fuel, self.distance))
