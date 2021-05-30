cd3fb449a119766e344e6bb722851947700114f5
Doa Roys <doa.roys@my.jcu.edu.au>
2021-05-30 15:51:38 +1000

    Use page().

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,8 +3,10 @@ finished = True
 user_input = input("Search:")
 while user_input != '':
     try:
-        search = wikipedia.summary(user_input)
-        print(search)
+        search = wikipedia.page(user_input)
+        print(search.title)
+        print(search.summary)
+        print(search.url)
     except wikipedia.exceptions.DisambiguationError as e:
         print (e.options)
     user_input = input("Search:")
